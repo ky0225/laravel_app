32017140b88ed88141cc63d88af07ac4c8139444
yamamoto <yamamoto.k@marietta.co.jp>
2021-12-08 12:32:23 +0900

    [Add]復元完了

--- laravel-app/app/Http/Middleware/Authenticate.php
+++ laravel-app/app/Http/Middleware/Authenticate.php
@@ -3,19 +3,35 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+//use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
-    }
+	// プロパティ
+	protected $user_route = 'user.login';
+	protected $owner_route = 'owner.login';
+	protected $admin_route = 'admin.login';
+
+	/**
+	 * Get the path the user should be redirected to when they are not authenticated.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return string|null
+	 */
+	protected function redirectTo($request)
+	{
+		if (!$request->expectsJson()) {
+			// ユーザー認証ができていないとき、ログイン画面にリダイレクト処理
+			if (Route::is('user.*')) {
+				return route($this->user_route);
+			}
+			if (Route::is('owner.*')) {
+				return route($this->owner_route);
+			}
+			if (Route::is('admin.*')) {
+				return route($this->admin_route);
+			}
+		}
+	}
 }
--- laravel-app/app/Providers/RouteServiceProvider.php
+++ laravel-app/app/Providers/RouteServiceProvider.php
@@ -10,54 +10,74 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to the "home" route for your application.
-     *
-     * This is used by Laravel authentication to redirect users after login.
-     *
-     * @var string
-     */
-    public const HOME = '/dashboard';
+	/**
+	 * The path to the "home" route for your application.
+	 *
+	 * This is used by Laravel authentication to redirect users after login.
+	 *
+	 * @var string
+	 */
+	// 各ユーザーのログイン後のルートを設定
+	public const HOME = '/dashboard';
+	public const OWNER_HOME = '/owner/dashboard';
+	public const ADMIN_HOME = '/admin/dashboard';
 
-    /**
-     * The controller namespace for the application.
-     *
-     * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
-     */
-    // protected $namespace = 'App\\Http\\Controllers';
+	/**
+	 * The controller namespace for the application.
+	 *
+	 * When present, controller route declarations will automatically be prefixed with this namespace.
+	 *
+	 * @var string|null
+	 */
+	// protected $namespace = 'App\\Http\\Controllers';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->configureRateLimiting();
+	/**
+	 * Define your route model bindings, pattern filters, etc.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->configureRateLimiting();
 
-        $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+		$this->routes(function () {
+			Route::prefix('api')
+				->middleware('api')
+				->namespace($this->namespace)
+				->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
-        });
-    }
+			// user のルート設定
+			Route::prefix('/') // URL の最初の文字列を指定
+				->as('user.') // ルートに別名をつける(記述が楽になる）
+				->middleware('web') // ユーザーの認証確認
+				->namespace($this->namespace) // 名前空間
+				->group(base_path('routes/web.php')); // 指定のファイルに全ての設定を適用させる
 
-    /**
-     * Configure the rate limiters for the application.
-     *
-     * @return void
-     */
-    protected function configureRateLimiting()
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
-        });
-    }
+			// admin のルート設定
+			Route::prefix('admin')
+				->as('admin.')
+				->middleware('web')
+				->namespace($this->namespace)
+				->group(base_path('routes/admin.php'));
+
+			// user のルート設定
+			Route::prefix('owner')
+				->as('owner.')
+				->middleware('web')
+				->namespace($this->namespace)
+				->group(base_path('routes/owner.php'));
+		});
+	}
+
+	/**
+	 * Configure the rate limiters for the application.
+	 *
+	 * @return void
+	 */
+	protected function configureRateLimiting()
+	{
+		RateLimiter::for('api', function (Request $request) {
+			return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+		});
+	}
 }
