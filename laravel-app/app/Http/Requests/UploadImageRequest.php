f766f7283c31762e1f55cf088e84094b55088e8b
yamamoto <yamamoto.k@marietta.co.jp>
2021-12-09 09:33:03 +0900

    [Add]Imageのバリデーション設定

--- laravel-app/app/Http/Requests/UploadImageRequest.php
+++ laravel-app/app/Http/Requests/UploadImageRequest.php
@@ -6,25 +6,37 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UploadImageRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	// 認証されているユーザーが使えるかどうか（基本的には true）
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	// バリデーションルール（今回は拡張子と容量）
+	public function rules()
+	{
+		return [
+			'image' => 'image|mines:jpg,jpeg,png|max:2048'
+		];
+	}
+
+	// エラーメッセージ
+	public function messages()
+	{
+		return [
+			'image' => '指定されたファイルが画像ではありません。',
+			'mines' => '指定された拡張子（jpg/jpeg/png）ではありません。',
+			'max' => 'ファイルサイズは2MB以内にしてください。',
+		];
+	}
 }
