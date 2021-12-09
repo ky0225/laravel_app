456f8582c029f46ddc52c1cab1130dfeeabac1d3
yamamoto <yamamoto.k@marietta.co.jp>
2021-12-09 10:06:38 +0900

    [Add]Imageのcreateとバリデーション

--- laravel-app/app/Http/Controllers/Owner/ImageController.php
+++ laravel-app/app/Http/Controllers/Owner/ImageController.php
@@ -5,15 +5,15 @@ namespace App\Http\Controllers\Owner;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Image;
-
+use App\Http\Requests\UploadImageRequest;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
 
 	// コントローラーでもユーザー認証を設定する
 	public function __construct()
@@ -21,78 +21,78 @@ class ImageController extends Controller
 		$this->middleware('auth:owners');
 	}
 
-    public function index()
-    {
-			$images = Image::where('id', '1')
+	public function index()
+	{
+		$images = Image::where('id', '1')
 			->orderBy('created_at', 'desc')
 			->paginate(20);
 
-			return view('owner.images.index', compact('images'));
-    }
+		return view('owner.images.index', compact('images'));
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('owner.images.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(UploadImageRequest $request)
+	{
+		dd($request);
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
--- laravel-app/app/Http/Requests/UploadImageRequest.php
+++ laravel-app/app/Http/Requests/UploadImageRequest.php
@@ -26,7 +26,7 @@ class UploadImageRequest extends FormRequest
 	public function rules()
 	{
 		return [
-			'image' => 'image|mines:jpg,jpeg,png|max:2048'
+			'image' => 'image|mimes:jpg,jpeg,png|max:2048'
 		];
 	}
 
@@ -35,7 +35,7 @@ class UploadImageRequest extends FormRequest
 	{
 		return [
 			'image' => '指定されたファイルが画像ではありません。',
-			'mines' => '指定された拡張子（jpg/jpeg/png）ではありません。',
+			'mimes' => '指定された拡張子（jpg/jpeg/png）ではありません。',
 			'max' => 'ファイルサイズは2MB以内にしてください。',
 		];
 	}
