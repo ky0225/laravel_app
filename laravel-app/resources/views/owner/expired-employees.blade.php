ade1b39f9cee1d6feee2a9fdb361fb17e12c514c
yamamoto <yamamoto.k@marietta.co.jp>
2021-12-10 12:37:41 +0900

    [Add]削除済み社員のview

--- laravel-app/resources/views/owner/expired-employees.blade.php
+++ laravel-app/resources/views/owner/expired-employees.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 	<x-slot name="header">
 		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-			削除済みオーナー一覧
+			削除済み社員一覧
 		</h2>
 	</x-slot>
 
@@ -17,23 +17,30 @@
 									<table class="table-auto w-full text-left whitespace-no-wrap">
 										<thead>
 										<tr>
-											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
+											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">社員ID</th>
+											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">所属</th>
+											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">拠点</th>
+											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">姓</th>
+											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">名</th>
 											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
-											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">期限が切れた日</th>
 											<th class="ma:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
 										</tr>
 										</thead>
 										<tbody>
-											@foreach($expiredOwners as $expiredOwner)
+											@foreach($expiredEmployees as $expiredEmployee)
 											<tr>
-												<td class="ma:px-4 py-3">{{ $expiredOwner->name }}</td>
-												<td class="ma:px-4 py-3">{{ $expiredOwner->email }}</td>
-												<td class="ma:px-4 py-3">{{ $expiredOwner->deleted_at->diffForHumans() }}</td>
-												<form id="delete_{{ $expiredOwner->id }}" method="post" action="{{ route('admin.expired-owners.destroy', ['owner' => $expiredOwner->id]) }}">
+												<td class="ma:px-4 py-3">{{ $expiredEmployee->id }}</td>
+												<td class="ma:px-4 py-3">{{ $expiredEmployee->organization->name }}</td>
+												<td class="ma:px-4 py-3">{{ $expiredEmployee->base->name }}</td>
+												<td class="ma:px-4 py-3">{{ $expiredEmployee->last_name }}</td>
+												<td class="ma:px-4 py-3">{{ $expiredEmployee->first_name }}</td>
+												<td class="ma:px-4 py-3">{{ $expiredEmployee->email }}</td>
+												<td class="ma:px-4 py-3">{{ $expiredEmployee->deleted_at->diffForHumans() }}</td>
+												<form id="delete_{{ $expiredEmployee->id }}" method="post" action="{{ route('owner.expired-employees.destroy', ['employee' => $expiredEmployee->id]) }}">
 													{{-- ルーティングでdestroyをPOST通信するよう記述しているため、@method="delete" の記述は不要（エラー発生） --}}
 													@csrf
 													<td class="ma:px-4 py-3">
-														<a href="#" data-id="{{ $expiredOwner->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded">完全に削除</a>
+														<a href="#" data-id="{{ $expiredEmployee->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded">完全に削除</a>
 													</td>
 												</form>
 											</tr>
