43affd93dc6eb32b959702eb9e062b33f58917cf
yamamoto <yamamoto.k@marietta.co.jp>
2021-12-14 16:04:15 +0900

    [Modify]view調整

--- laravel-app/resources/views/owner/employees/index.blade.php
+++ laravel-app/resources/views/owner/employees/index.blade.php
@@ -13,7 +13,7 @@
 					<section class="text-gray-600 body-font">
 						<div class="container md:px-5 mx-auto">
 							<x-flash-message status="session('status')"></x-flash-message>
-							<div class="flex justify-end mb-4">
+							<div class="flex justify-end mb-4 mt-4">
 								<button onclick="location.href='{{ route('owner.employees.create') }}'" class="text-white bg-indigo-500 border-0 mr-4 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
 								<button onclick="location.href='{{ route('owner.csv.index') }}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-md">CSVファイルで一括登録する</button>
 							</div>
